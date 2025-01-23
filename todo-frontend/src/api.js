3e2e4feb041aad9aaf36870d69521a16269c003c
Amjad0202 <mamamjadali@gmail.com>
2025-01-23 22:11:42 +0530

    Implement API functions for task management in frontend

---
+++ todo-frontend/src/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api/tasks';
+
+// Fetch all tasks
+export const fetchTasks = async () => {
+  const response = await axios.get(API_BASE_URL);
+  return response.data;
+};
+
+// Add a new task
+export const addTask = async (task) => {
+  const response = await axios.post(API_BASE_URL, task);
+  return response.data;
+};
+
+// Update a task
+export const updateTask = async (id, updatedTask) => {
+  const response = await axios.put(`${API_BASE_URL}/${id}`, updatedTask);
+  return response.data;
+};
+
+// Delete a task
+export const deleteTask = async (id) => {
+  await axios.delete(`${API_BASE_URL}/${id}`);
+};
